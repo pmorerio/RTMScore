a9370f39ba5cc6a1b9372c5d7d075ea0f33f448e
Pietro Morerio <pietro.morerio@iit.it>
2022-12-06 15:21:16 +0000

    [rtmscore] DUDE eval script

--- example/predict_DUDE.sh
+++ example/predict_DUDE.sh
@@ -2,8 +2,9 @@
 mkdir results_DUDE
 for complex_folder in $1/* 
 do
-	echo ${complex_folder:16}
+	DIR=$(basename "$complex_folder")
+	echo $DIR
 	echo ${complex_folder}/pocket.pdb
-	python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/LIGEN-keysites/DSCORE_pose.mol2  -m ../trained_models/rtmscore_model1.pth -pl -o "./results_DUDE/${complex_folder:16}"
+	python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/ligen_poses.mol2  -m ../trained_models/rtmscore_model1.pth -pl -o "./results_DUDE/$DIR"
 
 	done
