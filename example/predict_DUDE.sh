bf890c9487cc502a1c766ba7f72b512a7c02dcbe
Pietro Morerio <pietro.morerio@iit.it>
2022-12-13 14:16:55 +0000

    DUDE eval  out folder

--- example/predict_DUDE.sh
+++ example/predict_DUDE.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-mkdir results_DUDE
-for complex_folder in $1/* 
+OUT=results_DUDE
+mkdir $OUT
+for complex_folder in $1/*
 do
 	DIR=$(basename "$complex_folder")
 	echo $DIR
 	echo ${complex_folder}/pocket.pdb
-	python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/ligen_poses.mol2  -m ../trained_models/rtmscore_model1.pth -pl -o "./results_DUDE/$DIR"
+	python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/ligen_poses.mol2  -m ../trained_models/rtmscore_model1.pth -pl -o "./$OUT/$DIR"
+	#python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/plants_poses.mol2  -m ../trained_models/rtmscore_model1.pth -pl -o "./$OUT/$DIR"
 
 	done
