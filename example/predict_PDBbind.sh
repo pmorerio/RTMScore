11f9488a02af7f1697b589e80f5bbe8fb404b0a6
Pietro Morerio <pietro.morerio@iit.it>
2023-01-16 08:46:37 +0000

    sdf for PDBBind

--- example/predict_PDBbind.sh
+++ example/predict_PDBbind.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 OUT=results_PDB
 mkdir $OUT
-for complex_folder in $1/*
+for complex_folder in $1/*19436_8GPB*
 do
 	DIR=$(basename "$complex_folder")
 	echo $DIR
 	echo ${complex_folder}/pocket.pdb
-	python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/ligen_poses.mol2  -m ../trained_models/rtmscore_model1.pth -pl -o "./$OUT/$DIR"
+	python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/ligen_poses.sdf  -m ../trained_models/rtmscore_model1.pth -pl -o "./$OUT/$DIR"
 	#python rtmscore.py -p ${complex_folder}/pocket.pdb -l ${complex_folder}/plants_poses.mol2  -m ../trained_models/rtmscore_model1.pth -pl -o "./$OUT/$DIR"
 
 	done
